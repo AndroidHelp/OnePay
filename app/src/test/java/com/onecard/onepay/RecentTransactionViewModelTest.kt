c894cfce9ff694251accc17fc421f13c5b06128e
Hemant Katariya <android.developer20@gmail.com>
2020-09-01 14:16:09 +0530

    Unit Testing Mock repo and verify with viewmodel, test live data.

--- app/src/test/java/com/onecard/onepay/RecentTransactionViewModelTest.kt
+++ app/src/test/java/com/onecard/onepay/RecentTransactionViewModelTest.kt
@@ -3,31 +3,37 @@ package com.onecard.onepay
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.nhaarman.mockitokotlin2.mock
-import com.onecard.onepay.di.repoModule
-import com.onecard.onepay.di.viewModelModule
 import com.onecard.onepay.gpay.RecentTransRepository
 import com.onecard.onepay.gpay.RecentTransViewModel
 import com.onecard.onepay.gpay.RecentTransactionData
+import com.onecard.onepay.gpay.RecentTransactionItem
 import com.onecard.onepay.util.Resource
-import com.onecard.onepay.util.Status
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
-import org.koin.test.inject
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
 
 class RecentTransactionViewModelTest : KoinTest {
 
-    val recentTransViewModel: RecentTransViewModel by inject()
-    val recentTransRepository: RecentTransRepository by inject()
+//    val recentTransViewModel: RecentTransViewModel by inject()
+//    val recentTransRepository: RecentTransRepository by inject()
+
+    val recentTransData = ArrayList<RecentTransactionItem>()
+    private lateinit var recentTransViewModel: RecentTransViewModel
+
+    @Mock
+    lateinit var recentTransRepository: RecentTransRepository
 
     @Mock
     lateinit var recentTransObserver: Observer<Resource<RecentTransactionData>>
@@ -40,9 +46,13 @@ class RecentTransactionViewModelTest : KoinTest {
 
     @Before
     fun before() {
+        MockitoAnnotations.initMocks(this)
         recentTransObserver = mock()
-        startKoin { modules(viewModelModule, repoModule) }
+        //   startKoin { modules(viewModelModule, repoModule) }
         Dispatchers.setMain(mainThreadSurrogate)
+        recentTransViewModel = RecentTransViewModel(recentTransRepository)
+        recentTransViewModel.recentTransData.observeForever(recentTransObserver)
+
     }
 
     @After
@@ -52,15 +62,45 @@ class RecentTransactionViewModelTest : KoinTest {
         mainThreadSurrogate.close()
     }
 
+    /*  @Test
+      fun getRecentTransactionsSuccess() = runBlocking {
+          recentTransViewModel.recentTransData.observeForever(recentTransObserver)
+          recentTransViewModel.fetchRecentTrans()
+          delay(10)
+          val value = recentTransViewModel.recentTransData.value
+              ?: recentTransRepository.getRecentTransaction()
+          verify(recentTransObserver).onChanged(value)
+          assert(recentTransViewModel.recentTransData.value!!.status == Status.SUCCESS)
+      }*/
+
     @Test
-    fun getRecentTransactionsSuccess() = runBlocking {
-        recentTransViewModel.recentTransData.observeForever(recentTransObserver)
+    fun getRecentTransactionSuccess() = runBlocking {
+        `when`(recentTransRepository.getRecentTransaction()).thenReturn(
+            Resource.success(
+                RecentTransactionData(recentTransData)
+            )
+        )
         recentTransViewModel.fetchRecentTrans()
         delay(10)
-        val value = recentTransViewModel.recentTransData.value
-            ?: recentTransRepository.getRecentTransaction()
-        verify(recentTransObserver).onChanged(value)
-        assert(recentTransViewModel.recentTransData.value!!.status == Status.SUCCESS)
+        assertNotNull(recentTransViewModel.recentTransData.value)
+        assertEquals(recentTransRepository.getRecentTransaction().status, recentTransViewModel.recentTransData.value?.status)
+        verify(recentTransObserver).onChanged(recentTransViewModel.recentTransData.value)
+        println(recentTransViewModel.recentTransData.value!!.data!!.recentTransactionData.size)
+    }
+
+    @Test
+    fun getRecentTransactionFailed() = runBlocking {
+        `when`(recentTransRepository.getRecentTransaction()).thenReturn(
+            Resource.error("error",
+               null
+            )
+        )
+        recentTransViewModel.fetchRecentTrans()
+        delay(10)
+        assertNotNull(recentTransViewModel.recentTransData.value)
+        assertEquals(recentTransRepository.getRecentTransaction().status, recentTransViewModel.recentTransData.value?.status)
+        verify(recentTransObserver).onChanged(recentTransViewModel.recentTransData.value)
+//        println(recentTransViewModel.recentTransData.value!!.data!!.recentTransactionData.size)
     }
 
 }
